7cafc1c32aeb49bae92ff667a8af9524106b12b5
Ypurek <oleksii.ostapov@gmail.com>
2020-12-29 23:18:06 +0200

    styles ready, lightAjax added, coordinates added

--- assets/js/lightAjax.js
+++ assets/js/lightAjax.js
@@ -1,63 +1,50 @@
 class LightAjax {
-    constructor(token) {
-        this.xhr = new XMLHttpRequest();
-        this.xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.setRequestHeader('X-CSRFToken', token);
+    static token;
+
+    /*
+responseType: default "text", "arraybuffer", "blob", "document" (xml), "json"
+handleResponse: takes xhr as argument
+ */
+    static #prepareRequest(method, url, handleResponse, responseType) {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        if (responseType == null)
+            xhr.responseType = "json";
+        else
+            xhr.responseType = responseType;
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('X-CSRFToken', this.token);
+        xhr.onload = function () {
+            handleResponse(xhr.status, xhr.response);
+        }
+        xhr.onerror = function () {
+            console.error('request failed: ' + xhr.statusText)
+        }
+        return xhr;
     }
 
-    get(url) {
-        this.xhr.open('GET', url);
-        this.xhr.send()
+    static get(url, handleResponse, responseType) {
+        let xhr = this.#prepareRequest('GET', url, handleResponse, responseType);
+        xhr.send();
     }
 
-    post(url, payload) {
-        this.xhr.open("POST", url);
-        xhr.send(JSON.stringify(payload));
+    static post(url, payload, handleResponse, responseType) {
+        let xhr = this.#prepareRequest("POST", url, handleResponse, responseType);
+        xhr.send(payload);
+    }
+
+    static delete(url, handleResponse, responseType) {
+        let xhr = this.#prepareRequest('DELETE', url, handleResponse, responseType);
+        xhr.send();
+    }
+
+    static patch(url, payload, handleResponse, responseType) {
+        let xhr = this.#prepareRequest("PATCH", url, handleResponse, responseType);
+        xhr.send(payload);
+    }
+
+    static put(url, payload, handleResponse, responseType) {
+        let xhr = this.#prepareRequest("PUT", url, handleResponse, responseType);
+        xhr.send(payload);
     }
 }
-
-// <script>
-//     // code from https://developers.google.com/web/fundamentals/native-hardware/user-location?hl=ru&authuser=0
-//     $(document).ready(function () {
-//         if (navigator.geolocation) {
-//             console.log('Geolocation is supported!');
-//         } else {
-//             console.error('Geolocation is not supported for this Browser/OS version yet.');
-//         }
-//         ;
-//         let geoOptions = {
-//             maximumAge: 5 * 60 * 1000,
-//         }
-//         let geoSuccess = function (position) {
-//             let x = Number(position.coords.latitude).toFixed(3);
-//             let y = Number(position.coords.longitude).toFixed(3);
-//             console.log('latitude ' + x + 'longitude ' + y);
-//             $('.latitude').text(x);
-//             $('.longitude').text(y);
-//
-//         };
-//         let geoError = function (position) {
-//             let message = ''
-//             switch (position.code) {
-//                 case 0:
-//                     message = 'unknown error';
-//                     break;
-//                 case 1:
-//                     message = 'permission denied';
-//                     break;
-//                 case 2:
-//                     message = 'position unavailable';
-//                     break;
-//                 case 3:
-//                     message = 'timeout';
-//                     break;
-//                 default:
-//                     message = 'shit happened'
-//             }
-//             console.error('Error occurred: ' + message);
-//         };
-//         navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
-//     })
-//
-// </script>
-
