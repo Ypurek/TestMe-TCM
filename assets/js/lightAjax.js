50c66e8b71845b74bf0f60047a4b77ffd3dfd728
Ypurek <oleksii.ostapov@gmail.com>
2021-02-06 09:05:11 +0200

    make prepareRequest not private
    It appears Firefox 84.0.1 and Playwright Webkit 1.1 do not support JS private methods in classes

--- assets/js/lightAjax.js
+++ assets/js/lightAjax.js
@@ -5,7 +5,7 @@ class LightAjax {
 responseType: default "text", "arraybuffer", "blob", "document" (xml), "json"
 handleResponse: takes xhr as argument
  */
-    static #prepareRequest(method, url, handleResponse, responseType) {
+    static prepareRequest(method, url, handleResponse, responseType) {
         let xhr = new XMLHttpRequest();
         xhr.open(method, url);
         if (responseType == null)
@@ -25,13 +25,13 @@ handleResponse: takes xhr as argument
     }
 
     static get(url, handleResponse, responseType) {
-        let xhr = this.#prepareRequest('GET', url, handleResponse, responseType);
+        let xhr = this.prepareRequest('GET', url, handleResponse, responseType);
         xhr.send();
         return xhr;
     }
 
     static post(url, payload, handleResponse, responseType) {
-        let xhr = this.#prepareRequest("POST", url, handleResponse, responseType);
+        let xhr = this.prepareRequest("POST", url, handleResponse, responseType);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(payload);
         return xhr;
@@ -40,7 +40,7 @@ handleResponse: takes xhr as argument
     static file_upload(url, file, handleResponse, handleProgress) {
         let formData = new FormData();
         formData.append('file', file)
-        let xhr = this.#prepareRequest("POST", url, handleResponse, '');
+        let xhr = this.prepareRequest("POST", url, handleResponse, '');
         // xhr.onloadend = function () {
         //     handleResponse(xhr.status, xhr.response)
         // }
@@ -53,20 +53,20 @@ handleResponse: takes xhr as argument
     }
 
     static delete(url, handleResponse, responseType) {
-        let xhr = this.#prepareRequest('DELETE', url, handleResponse, responseType);
+        let xhr = this.prepareRequest('DELETE', url, handleResponse, responseType);
         xhr.send();
         return xhr;
     }
 
     static patch(url, payload, handleResponse, responseType) {
-        let xhr = this.#prepareRequest("PATCH", url, handleResponse, responseType);
+        let xhr = this.prepareRequest("PATCH", url, handleResponse, responseType);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(payload);
         return xhr;
     }
 
     static put(url, payload, handleResponse, responseType) {
-        let xhr = this.#prepareRequest("PUT", url, handleResponse, responseType);
+        let xhr = this.prepareRequest("PUT", url, handleResponse, responseType);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(payload);
         return xhr;
